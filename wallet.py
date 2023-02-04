0e3f61d5ca1d0162208fc141869eea84259b1728
Zaafir Rizwan <108717727+ZaafirRizwan@users.noreply.github.com>
2023-02-04 15:25:57 +0500

    update

--- wallet.py
+++ wallet.py
@@ -1,1 +1,13 @@
-
+class Wallet:
+  def __init__(self, balance):
+      self.balance = balance
+      
+  def setamount(self,balance):
+      self.balance = balance
+      
+      
+  def getamount(self,balance):
+      return self.balance
+    
+  def removeamount(self,balance):
+      self.balance = 0
